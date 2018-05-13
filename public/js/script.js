e9162221084732f32ef58603ad136fbea631c2f7
esteban escobar <esteban203@outlook.com>
2018-05-12 19:49:12 -0500

    Base de datos corregida

---
+++ public/js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+ 
+	$(window).scroll(function(){
+		var barra = $(window).scrollTop();
+		var posicion =  (barra * 0.40);
+		
+		$('.slide').css({
+			'background-position': '0 -' + posicion + 'px'
+		});
+
+		$('.promo').css({
+			'background-position': '0 -' + posicion + 'px'
+		});
+ 
+		$('.lineas').css({
+			'background-position': '0 -' + posicion + 'px'
+		});
+
+		$('.carac').css({
+			'background-position': '0 -' + posicion  + 'px'
+		});
+
+		$('.imagenes').css({
+			'background-position': '0 -' + posicion  + 'px'
+		});
+ 
+	});
+ 
+});
+
